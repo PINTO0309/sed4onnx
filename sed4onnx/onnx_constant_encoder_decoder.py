07c1d5049a7be7a6102ff8e83a74ea6ec16f6e84
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-31 00:53:30 +0900

    Support for "float16", "int16", "string"

--- sed4onnx/onnx_constant_encoder_decoder.py
+++ sed4onnx/onnx_constant_encoder_decoder.py
@@ -33,12 +33,15 @@ class Color:
     RESET          = '\033[0m'
 
 DTYPES_TO_NUMPY_DTYPES: dict = {
+    'float16': np.float16,
     'float32': np.float32,
     'float64': np.float64,
     'uint8': np.uint8,
     'int8': np.int8,
+    'int16': np.int16,
     'int32': np.int32,
     'int64': np.int64,
+    'string': np.string_,
 }
 
 
@@ -54,7 +57,7 @@ def encode(
         ASCII string to be encoded.
 
     dtype: str
-        'float32' or 'float64' or 'uint8' or 'int8' or 'int32' or 'int64'
+        'float16' or 'float32' or 'float64' or 'uint8' or 'int8' or 'int16' or 'int32' or 'int64' or 'string'
 
     Returns
     -------
@@ -71,13 +74,20 @@ def encode(
         sys.exit(1)
 
     # Return
-    return base64.b64encode(
-        np.asarray(
-            ast.literal_eval(constant_string),
-            dtype=DTYPES_TO_NUMPY_DTYPES[dtype],
-        ).tobytes()
-    ).decode('utf-8')
-
+    if dtype != 'string':
+        return base64.b64encode(
+            np.asarray(
+                ast.literal_eval(constant_string),
+                dtype=DTYPES_TO_NUMPY_DTYPES[dtype],
+            ).tobytes()
+        ).decode('utf-8')
+    else:
+        return base64.b64encode(
+            np.asarray(
+                constant_string,
+                dtype=DTYPES_TO_NUMPY_DTYPES[dtype],
+            ).tobytes()
+        ).decode('utf-8')
 
 def decode(
     constant_string: str,
@@ -91,7 +101,7 @@ def decode(
         Base64 string to be decoded.
 
     dtype: str
-        'float32' or 'float64' or 'uint8' or 'int8' or 'int32' or 'int64'
+        'float16' or 'float32' or 'float64' or 'uint8' or 'int8' or 'int16' or 'int32' or 'int64' or 'string'
 
     Returns
     -------
@@ -108,10 +118,13 @@ def decode(
         sys.exit(1)
 
     # Return
-    return np.frombuffer(
-        base64.b64decode(constant_string),
-        dtype=DTYPES_TO_NUMPY_DTYPES[dtype]
-    )
+    if dtype != 'string':
+        return np.frombuffer(
+            base64.b64decode(constant_string),
+            dtype=DTYPES_TO_NUMPY_DTYPES[dtype]
+        )
+    else:
+        return base64.b64decode(constant_string).decode('utf-8')
 
 
 def main():
@@ -141,13 +154,14 @@ def main():
         help=\
             'encode: Converts the string specified in constant_string to a Base64 format string '+
             'that can be embedded in ONNX constants. \n'+
-            'decode: Converts a Base64 string specified in constant_string to ASCII like Numpy string.'
+            'decode: Converts a Base64 string specified in constant_string to ASCII like '+
+            'Numpy string or pure string.'
     )
     args = parser.parse_args()
 
-    constant_string = args.constant_string
-    dtype = args.dtype
-    mode = args.mode
+    constant_string: str = args.constant_string
+    dtype: str = args.dtype
+    mode: str = args.mode
 
     if not dtype:
         print(
@@ -168,16 +182,19 @@ def main():
             constant_string=constant_string,
             dtype=dtype,
         )
-        decoded_ndarray_str = re.sub(
-            pattern=r'\s+',
-            repl='',
-            string=np.array2string(
-                decoded_ndarray,
-                threshold=np.inf,
-                max_line_width=np.inf,
-                separator=','
+        if dtype != 'string':
+            decoded_ndarray_str = re.sub(
+                pattern=r'\s+',
+                repl='',
+                string=np.array2string(
+                    decoded_ndarray,
+                    threshold=np.inf,
+                    max_line_width=np.inf,
+                    separator=','
+                )
             )
-        )
+        else:
+            decoded_ndarray_str = decoded_ndarray
         print(decoded_ndarray_str)
 
 
